c8eb62cdc3c7cd5c6ce58a7522069d30791f8884
Wiktor Ozga <lokiuz@gmail.com>
2018-01-27 18:48:47 +0100

    expose createBlockRenderer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import RawParser from './RawParser';
 import createStylesRenderer from './createStyleRenderer';
 import { renderNode, render } from './render';
 import CompositeDecorator from './helpers/CompositeDecorator';
+import createBlockRenderer from './createBlockRenderer';
 
 export {
   createStylesRenderer,
+  createBlockRenderer,
   RawParser,
   renderNode,
   CompositeDecorator,
