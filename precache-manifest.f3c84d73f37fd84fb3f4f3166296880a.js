6046c194f3560025b93e870065c931012aba99f1
Wiktor Ożga <lokiuz@gmail.com>
2019-07-01 23:36:57 +0200

    Updates

---
+++ precache-manifest.f3c84d73f37fd84fb3f4f3166296880a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a42fa53c1f092bcee82e9f57953721",
+    "url": "/redraft/index.html"
+  },
+  {
+    "revision": "36f2de48d08c3d17e849",
+    "url": "/redraft/static/css/2.ef824610.chunk.css"
+  },
+  {
+    "revision": "3070d037223d3ed70adc",
+    "url": "/redraft/static/css/main.ccb796d5.chunk.css"
+  },
+  {
+    "revision": "36f2de48d08c3d17e849",
+    "url": "/redraft/static/js/2.8a5bcc48.chunk.js"
+  },
+  {
+    "revision": "3070d037223d3ed70adc",
+    "url": "/redraft/static/js/main.9eb9cfee.chunk.js"
+  },
+  {
+    "revision": "0c95e4ea74373cb08e2c",
+    "url": "/redraft/static/js/runtime~main.75704451.js"
+  }
+]);
