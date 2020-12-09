94f334922add42bf59d888fc8688c198eecf8fb2
shaulg <shaulgo@gmail.com>
2020-12-09 22:42:55 +0200

    fix publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -95,8 +95,7 @@ function publishPackage(pkg) {
 function run() {
   let skip;
   const { FORCE_PUBLISH, GITHUB_REF, CI } = process.env;
-  console.log(GITHUB_REF);
-  if ((!GITHUB_REF || GITHUB_REF.indexOf('master') === -1) && !FORCE_PUBLISH) {
+  if ((!GITHUB_REF || GITHUB_REF.indexOf('release') === -1) && !FORCE_PUBLISH) {
     skip = 'Not on master branch';
   } else if (!CI) {
     skip = 'Not in CI';
